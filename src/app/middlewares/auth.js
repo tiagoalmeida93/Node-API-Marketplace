7b76847afd6301ea99cebebe741103332ebba73e
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-04-30 01:55:12 -0300

    Utilizando require-dir

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const authConfig = require("../../config/auth");
+const { promisify } = require("util");
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+    req.userName = decoded.name;
+    req.userEmail = decoded.email;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: "Token invalid" });
+  }
+};
