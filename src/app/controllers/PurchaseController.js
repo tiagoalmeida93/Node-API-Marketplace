858fb06a69226b2f00f3a642e01f114be5392f7f
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-05-02 01:02:11 -0300

    Desafio

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,4 +1,5 @@
 const Ad = require("../models/Ad");
+const Purchase = require("../models/Purchase");
 const User = require("../models/User");
 const PurchaseMail = require("../jobs/PurchaseMail");
 const Queue = require("../services/Queue");
@@ -10,13 +11,19 @@ class PurchaseController {
     const purchaseAd = await Ad.findById(ad).populate("author");
     const user = await User.findById(req.userId);
 
+    const purchase = await Purchase.create({
+      content,
+      ad,
+      user: user._id
+    });
+
     Queue.create(PurchaseMail.key, {
       ad: purchaseAd,
       user,
       content
     }).save();
 
-    return res.send();
+    return res.send(purchase);
   }
 }
 
